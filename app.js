29e9c0e1ee938683f1875d1a524d20ed8a841edd
moniquedare <102301343+moniquedare@users.noreply.github.com>
2022-04-08 21:28:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const { circleArea,squareArea } = require('./shape-area.js');
 const raio = process.argv[2];
 const areaOfCircle = circleArea(raio);
 
-console.log(`A área do círculo com esse raio é de ${areaOfCircle}`);
+console.log(`A área do círculo com esse raio é de: ${areaOfCircle}`);
 
 //
 
 const lado = process.argv[3];
 const areaOfSquare = squareArea(lado);
 
-console.log(`A área do quadrado com esse valor de lado é de ${areaOfSquare}`);
+console.log(`A área do quadrado com esse valor de lado é de: ${areaOfSquare}`);
